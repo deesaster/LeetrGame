6f41a48e630eb149602ab0a62c11d34fb254ff0d
Denis Smirnov <denis@deesastudio.com>
2011-06-04 10:45:27 -0700

    Added empty GoodShepherd game class

---
+++ leetrgame-base/src/com/leetr/goodshepherd/GoodShepherd.java
@@ -0,0 +1,12 @@
+package com.leetr.goodshepherd;
+
+import com.leetr.game.GdxGame;
+
+/**
+ * Created By: Denis Smirnov <denis@deesastudio.com>
+ * <p/>
+ * Date: 11-06-04
+ * Time: 10:44 AM
+ */
+public class GoodShepherd extends GdxGame {
+}
